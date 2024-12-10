cecebd5796aae322a017ff123dd875cbb6247041
SravankumrBachu <157828409+SravankumrBachu@users.noreply.github.com>
2024-12-10 11:46:04 +0530

    Create Logo.jsx

---
+++ simply react assignment/Logo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div className="square">
+        <div className="text">
+          <h1>HTML & CSS</h1>
+          <p>design and build websites</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
